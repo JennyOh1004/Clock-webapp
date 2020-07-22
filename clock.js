b2348e7fdaade9bfdd40d3521f9b04c6a54fdad5
JennyOh1004 <ohseulki1004@gmail.com>
2020-07-22 12:23:10 -0700

    Created html, css, js files

---
+++ clock.js
@@ -0,0 +1,10 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+}
+
+function init() {}
+
+init();
